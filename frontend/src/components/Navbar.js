4cbf36648529d3f419c7736d0ece759e5bf45411
unknown <chadiegilaugis@gmail.com>
2022-11-11 15:25:26 +0800

    added create and read

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
       <div className="container">
-        <Link to="/">Workout Buddy</Link>
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
       </div>
     </header>
   );
